9583b9ac52931497805c183292d1b0dc433fda46
Leandro Arturi <lea.arturi@gmail.com>
2022-07-26 16:08:03 -0300

    Sass y Bootstrap

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Twitter Clone
+
+## Clon de Twitter con React, Apollo, GraphQL & Mongo
+
+### 🚀 Quick start
+
+#### Getting Started with Create React App
+
+This project was bootstrapped with Vite.js.
+
+`npm install`
+
+`npm run dev`
+
+##### Made with ❤️ by Leandro Arturi
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+   const [user, setUser] = useState(null);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+   if (user) {
+      return (
+         <div>
+            <h1>Logueado</h1>
+         </div>
+      );
+   } else {
+      return (
+         <div>
+            <h1>Login</h1>
+            <Button variant="primary">Primary</Button>{' '}
+         </div>
+      );
+   }
 }
-
-export default App
