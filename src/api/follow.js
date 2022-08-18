264e3a8382097e2f1f214564e0058159e5c96b3e
Leandro Arturi <lea.arturi@gmail.com>
2022-08-18 18:08:38 -0300

    Sistema de Followers

---
+++ src/api/follow.js
@@ -0,0 +1,52 @@
+import { getTokenApi } from './auth';
+
+export function checkFollowApi(idUser) {
+   const url = `${import.meta.env.VITE_APP_API_URL}/relationCkeck?id=${idUser}`;
+
+   const params = {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         Authorization: `Bearer ${getTokenApi()}`,
+      },
+   };
+
+   return fetch(url, params)
+      .then(response => response.json())
+      .then(result => result)
+      .catch(err => err);
+}
+
+export function followUserApi(idUser) {
+   const url = `${import.meta.env.VITE_APP_API_URL}/relationAdd?id=${idUser}`;
+
+   const params = {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         Authorization: `Bearer ${getTokenApi()}`,
+      },
+   };
+
+   return fetch(url, params)
+      .then(response => response.json())
+      .then(result => result)
+      .catch(err => err);
+}
+
+export function unfollowUserApi(idUser) {
+   const url = `${import.meta.env.VITE_APP_API_URL}/relationRemove?id=${idUser}`;
+
+   const params = {
+      method: 'DELETE',
+      headers: {
+         'Content-Type': 'application/json',
+         Authorization: `Bearer ${getTokenApi()}`,
+      },
+   };
+
+   return fetch(url, params)
+      .then(response => response.json())
+      .then(result => result)
+      .catch(err => err);
+}
--- src/components/User/BannerAvatar/BannerAvatar.jsx
+++ src/components/User/BannerAvatar/BannerAvatar.jsx
@@ -1,45 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import avatarNotFound from '../../../assets/png/avatar-no-found.png';
 import ConfigModal from '../../Modal/ConfigModal';
 import EditUserForm from '../EditUserForm/EditUserForm';
+import { checkFollowApi, followUserApi, unfollowUserApi } from '../../../api/follow';
 
 import './BannerAvatar.scss';
 
 const BannerAvatar = ({ user, loggedUser }) => {
+   const [showModal, setShowModal] = useState(false);
+   const [isFollowing, setIsFollowing] = useState(null);
+   const [reloadFollow, setReloadFollow] = useState(false)
 
-    const [showModal, setShowModal] = useState(false);
+   const bannerUrl = user?.banner
+      ? `${import.meta.env.VITE_APP_API_URL}/getBanner?id=${user?.id}`
+      : null;
+   const avatarUrl =
+      user?.avatar != 'Mi Avatar' && user?.avatar != undefined
+         ? `${import.meta.env.VITE_APP_API_URL}/getAvatar?id=${user?.id}`
+         : avatarNotFound;
 
-    const bannerUrl = user?.banner ? `${import.meta.env.VITE_APP_API_URL}/getBanner?id=${user?.id}` : null;
-    const avatarUrl = (user?.avatar != 'Mi Avatar' && user?.avatar != undefined ) ? `${import.meta.env.VITE_APP_API_URL}/getAvatar?id=${user?.id}` : avatarNotFound;
-    
-    return (
-        <div 
-            className='banner-avatar' 
-            style={{ backgroundImage: `url('${bannerUrl}')` }}
-        >
-            <div className='avatar'  style={{ backgroundImage: `url('${avatarUrl}')` }} />
-
-            {
-                user && (
-                    <div className='options'>
-                        { loggedUser.user._id === user.id && (
-                            <Button onClick={() => setShowModal(true) }>Editar Perfil</Button>
-                        )}
-                        { loggedUser.user._id !== user.id && (<Button>Seguir</Button>) }
-                    </div>
-                )
+   useEffect(() => {
+      if(user) {
+        checkFollowApi(user?.id).then((response) => {
+            if (response?.status) {
+               setIsFollowing(true);
+            } else {
+               setIsFollowing(false);
             }
+         });
+         setReloadFollow(false);
+      }
+   }, [user, reloadFollow]);
 
-            <ConfigModal 
-                show={showModal} 
-                setShow={setShowModal}
-                title='Editar Perfil' 
-            >
-                <EditUserForm user={user} setShowModal={setShowModal} />
-            </ConfigModal>
-        </div>
-    );
-}
+   const onFollow = () => {
+      followUserApi(user?.id).then(() => {
+        setReloadFollow(true);
+      });
+   };
+
+   const onUnFollow = () => {
+      unfollowUserApi(user?.id).then(() => {
+        setReloadFollow(true);
+      });
+   };
+
+   return (
+      <div
+         className='banner-avatar'
+         style={{ backgroundImage: `url('${bannerUrl}')` }}
+      >
+         <div
+            className='avatar'
+            style={{ backgroundImage: `url('${avatarUrl}')` }}
+         />
+
+         {user && (
+            <div className='options'>
+               {loggedUser.user._id === user.id && (
+                  <Button onClick={() => setShowModal(true)}>
+                     Editar Perfil
+                  </Button>
+               )}
+
+               {loggedUser.user._id !== user.id && isFollowing !== null &&
+                  (isFollowing ? (
+                     <Button onClick={onUnFollow} className="unfollow"><span>Siguiendo</span></Button>
+                  ) : (
+                     <Button onClick={onFollow}>Seguir</Button>
+                  ))}
+            </div>
+         )}
+
+         <ConfigModal
+            show={showModal}
+            setShow={setShowModal}
+            title='Editar Perfil'
+         >
+            <EditUserForm user={user} setShowModal={setShowModal} />
+         </ConfigModal>
+      </div>
+   );
+};
 
 export default BannerAvatar;
