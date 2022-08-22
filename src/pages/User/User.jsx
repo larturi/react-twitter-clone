dc83132056562dd8a6c6a6783bdb9ac1db5f0d51
Leandro Arturi <lea.arturi@gmail.com>
2022-08-22 12:35:41 -0300

    Paginado de tweets

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -15,9 +15,27 @@ const User = () => {
     let { user_id } = useParams();
     const [userNameTitle, setUserNameTitle] = useState('...');
     const [user, setUser] = useState(null);
+    const [page, setPage] = useState(1);
+    const [loadingTweets, setLoadingTweets] = useState(false);
     const [tweets, setTweets] = useState(null);
 
     const loggedUser = useAuth();
+
+    const moreTweets = () => {
+        const pageTemp = page + 1;
+        setLoadingTweets(true);
+
+        getUserTweetsApi(user_id, pageTemp).then(response => {
+            console.log(response)
+            if(!response) {
+                setLoadingTweets(0);
+            } else {
+                setTweets([...tweets, ...response]);
+                setPage(pageTemp);
+                setLoadingTweets(false);
+            }
+        });
+    };
 
     useEffect(() => {
         getUserApi(user_id).then(response => {
@@ -54,9 +72,20 @@ const User = () => {
             <InfoUser user={user} />
             <div className='user__tweets'>
                 <h3>Tweets:</h3>
-                {
-                    tweets?.length > 0 ? <TweetsList tweets={tweets} /> : <p>El usuario aun no a creado tweets</p>
-                }
+                { tweets?.length > 0 ? <TweetsList tweets={tweets} /> : <p>El usuario aun no a creado tweets</p> }
+                <Button onClick={moreTweets}>
+                    { !loadingTweets ? (
+                        loadingTweets !== 0 && 'Obtener mas Tweets'
+                    ) : (
+                        <Spinner 
+                            as='span'
+                            animation='grow'
+                            size='sm'
+                            role='status'
+                            aria-hidden='true'
+                        />
+                    )}
+                </Button>
             </div>
         </>
     );
